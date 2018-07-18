509599d89513608f16bd1058c3176460396c7596
Patryk Swiatek <patrykswiatek@Air-Patryk.home>
2018-07-18 08:37:17 +0200

    Changes - stage2 effects

--- src/js/header.jsx
+++ src/js/header.jsx
@@ -13,12 +13,27 @@ import Link from "react-router-dom/es/Link";
 							<a>Sit <span>On</span> Chair</a>
 						</div>
 						<nav className='col-6'>
+							<div className='underline'>
 							<ul>
 								<li><Link to="/">HOME</Link></li>
 								<li><Link to="/contact">CONTACT</Link></li>
-								<li><a href="">DROPDOWN</a></li>
-								<li><a href="">ABOUT</a></li>
+								<li><Link to="">&dArr;DROPDOWN</Link>
+									<ul>
+										<li><a href="">ABOUT US</a></li>
+										<li><a href="">DEVELOPERS &rArr;</a>
+											<ul>
+												<li><a href="">OVERVIEW</a></li>
+												<li><a href="">DEVELOPERS TOOLS</a></li>
+												<li><a href="">DOCUMENTATION</a></li>
+											</ul>
+										</li>
+										<li><a href="">PARTNERSHIP</a></li>
+										<li><a href="">OFFERINGS</a></li>
+									</ul>
+								</li>
+								<li><Link to="">ABOUT</Link></li>
 							</ul>
+							</div>
 						</nav>
 					</header>
 				</div>
--- src/js/section1.jsx
+++ src/js/section1.jsx
@@ -1,75 +1,131 @@
-import React from 'react'
+import React from 'react';
 
 
-class Section1 extends React.Component {
+	class Section1 extends React.Component {
+		constructor(props) {
+			super(props);
 
-	render() {
+			this.state = {
+				hoverS: false, hoverSa: false, hoverSb: false,
+			}
+		}
 
-		return (<div className='section1'>
-			<div className='layer'>
-				<section className='row container'>
-					<div className='col-12 text'>
-						<div className='title'>
-							<div className='h1'>
-								<h1>LET'S TALK ABOUT YOUR COMFORT</h1>
+		mouseOnS = () => {
+			this.setState({
+				hoverS: true,
+			})
+		};
+		mouseOffS = () => {
+			this.setState({
+				hoverS: false,
+			})
+		};
+		mouseOnSa = () => {
+			this.setState({
+				hoverSa: true,
+			})
+		};
+		mouseOffSa = () => {
+			this.setState({
+				hoverSa: false,
+			})
+		};
+		mouseOnSb = () => {
+			this.setState({
+				hoverSb: true,
+			})
+		};
+		mouseOffSb = () => {
+			this.setState({
+				hoverSb: false,
+			})
+		};
+
+		render() {
+
+			const styleS = {
+				backgroundColor: this.state.hoverS ? 'black' : 'rgba(43, 43, 43, 0.88)',
+				color: this.state.hoverS ? 'orange' : 'whitesmoke',
+			};
+			const styleSa = {
+				backgroundColor: this.state.hoverSa ? 'black' : 'rgba(43, 43, 43, 0.88)',
+				color: this.state.hoverSa ? 'orange' : 'whitesmoke',
+			};
+			const styleSb = {
+				backgroundColor: this.state.hoverSb ? 'black' : 'rgba(43, 43, 43, 0.88)',
+				color: this.state.hoverSb ? 'orange' : 'whitesmoke',
+
+			};
+
+
+			return (<div className='section1'>
+				<div className='layer'>
+					<section className='row container'>
+						<div className='col-12 text'>
+							<div className='title'>
+								<div className='h1'>
+									<h1>LET'S TALK ABOUT YOUR COMFORT</h1>
+								</div>
+								<div className='a'>
+									<a>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</a>
+								</div>
 							</div>
-							<div className='a'>
-								<a>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</a>
+							<div className='boxes'>
+								<div className='box' onMouseOver={this.mouseOnS}
+								     onMouseLeave={this.mouseOffS} style={styleS}>
+									<span className='line'/>
+									<div className='h4'>
+										<h4>Donec ipsum massa</h4>
+									</div>
+									<div className='a1'>
+										<a>Donec vitae arcu. Etiam sapien elit, consequat eget,
+											tristique non, venenatis quis, ante. Nullam eget nisl.
+											Donec ipsum massa, ullamcorper in, auctor et,
+											scelerisque sed, est.</a>
+									</div>
+									<div className='a2'>
+										<a href="">Read More >></a>
+									</div>
+								</div>
+								<div className='box' onMouseOver={this.mouseOnSa}
+								     onMouseLeave={this.mouseOffSa} style={styleSa}>
+									<span className='line'/>
+									<div className='h4'>
+										<h4>Donec ipsum massa</h4>
+									</div>
+									<div className='a1'>
+										<a>Donec vitae arcu. Etiam sapien elit, consequat eget,
+											tristique non, venenatis quis, ante. Nullam eget nisl.
+											Donec ipsum massa, ullamcorper in, auctor et,
+											scelerisque sed, est.</a>
+									</div>
+									<div className='a2'>
+										<a href="">Read More >></a>
+									</div>
+								</div>
+
+								<div className='box' onMouseOver={this.mouseOnSb}
+								     onMouseLeave={this.mouseOffSb} style={styleSb}>
+									<span className='line'/>
+									<div className='h4'>
+										<h4>Donec ipsum massa</h4>
+									</div>
+									<div className='a1'>
+										<a>Donec vitae arcu. Etiam sapien elit, consequat eget,
+											tristique non, venenatis quis, ante. Nullam eget nisl.
+											Donec ipsum massa, ullamcorper in, auctor et,
+											scelerisque sed, est.</a>
+									</div>
+									<div className='a2'>
+										<a href="">Read More >></a>
+									</div>
+								</div>
 							</div>
 						</div>
-						<div className='boxes'>
-							<div className='box'>
-								<span className='line'/>
-								<div className='h4'>
-									<h4>Donec ipsum massa</h4>
-								</div>
-								<div className='a1'>
-									<a>Donec vitae arcu. Etiam sapien elit, consequat eget,
-										tristique non, venenatis quis, ante. Nullam eget nisl.
-										Donec ipsum massa, ullamcorper in, auctor et,
-										scelerisque sed, est.</a>
-								</div>
-								<div className='a2'>
-									<a href="">Read More >></a>
-								</div>
-							</div>
-							<div className='box'>
-								<span className='line'/>
-								<div className='h4'>
-									<h4>Donec ipsum massa</h4>
-								</div>
-								<div className='a1'>
-									<a>Donec vitae arcu. Etiam sapien elit, consequat eget,
-										tristique non, venenatis quis, ante. Nullam eget nisl.
-										Donec ipsum massa, ullamcorper in, auctor et,
-										scelerisque sed, est.</a>
-								</div>
-								<div className='a2'>
-									<a href="">Read More >></a>
-								</div>
-							</div>
-
-							<div className='box'>
-								<span className='line'/>
-								<div className='h4'>
-									<h4>Donec ipsum massa</h4>
-								</div>
-								<div className='a1'>
-									<a>Donec vitae arcu. Etiam sapien elit, consequat eget,
-										tristique non, venenatis quis, ante. Nullam eget nisl.
-										Donec ipsum massa, ullamcorper in, auctor et,
-										scelerisque sed, est.</a>
-								</div>
-								<div className='a2'>
-									<a href="">Read More >></a>
-								</div>
-							</div>
-						</div>
-					</div>
-				</section>
-			</div>
-		</div>);
+					</section>
+				</div>
+			</div>);
+		}
 	}
-}
 
 export default Section1;
